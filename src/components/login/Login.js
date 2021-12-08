137c6894268ade031ee1c879e7ddba265f5185f2
TranHongAnh2k <87012843+TranHongAnh2k@users.noreply.github.com>
2021-12-08 17:30:06 +0700

    commit

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
         <div className="login">
             <div className="login__container">
                 <h2>Youtube Clone</h2>
-                <img src="./images/logo.svg" alt="" />
+                <img src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" alt="" />
                 <button onClick={handleLogin}>Login With google</button>
             
                 <p>This Project is made using YOUTUBE DATA API</p>
